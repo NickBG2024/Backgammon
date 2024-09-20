ef34f6f7c1de89a4a532fd1bb12f6a21b2f59bcc
Nick <nickgroll@gmail.com>
2024-09-20 21:43:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,23 @@ st.title("SANDBOX: Backgammon Match Details parsing, via Email - subject: Admin:
 EMAIL = st.secrets["imap"]["email"]
 PASSWORD = st.secrets["imap"]["password"]
 
+
+
+test_subject = "Admin: A league match was played between MikeHagg (4 3 6.741 2.532) and NickBG (1 3 15.054 1.035) on Heroes!"
+
+# Adjusted regex to match the format correctly
+test_match = re.search(r"\(([^)]+)\)\s*and\s*\(([^)]+)\)", test_subject)
+
+if test_match:
+    test_player_1_values = test_match.group(1)  # Values for Player 1
+    test_player_2_values = test_match.group(2)  # Values for Player 2
+    
+    print(f"Player 1 values: {test_player_1_values}")
+    print(f"Player 2 values: {test_player_2_values}")
+else:
+    print("No match found.")
+
+
 try:
     mail = imaplib.IMAP4_SSL('mail.sabga.co.za', 993)  # Update to correct server
     mail.login(EMAIL, PASSWORD)
